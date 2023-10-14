39e0ad9d02e0658276160624696648029b6886bd
Marcus Redivo <mredivo@binarytool.com>
2023-10-14 15:06:56 -0700

    Fix make-all.sh to use full path to helper in same directory

--- bin/make-all.sh
+++ bin/make-all.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-for p in `list-all-projects.sh dirs`; do
+# Establish the repository base directory and root directory for scores
+REPO_BASE=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." &> /dev/null && pwd)
+
+for p in `$REPO_BASE/bin/list-all-projects.sh dirs`; do
+    echo "Visiting: $p"
     cd $p && make clean deps all
 done
