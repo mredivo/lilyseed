0f2cea519b205c72f249c0f57fdf7d02e20d123f
Marcus Redivo <mredivo@binarytool.com>
2023-10-19 19:22:38 -0700

    Make the compare-includedirs.sh script more robust

--- bin/compare-includedirs.sh
+++ bin/compare-includedirs.sh
@@ -4,12 +4,20 @@
 # the includes in the src/include/ directory.
 #
 
+# Establish the repository base directory and root directory for scores
+REPO_BASE=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." &> /dev/null && pwd)
+SCORE_ROOT=$REPO_BASE/src
+DIR_A="$REPO_BASE/template/include"
+DIR_B="$SCORE_ROOT/include"
+
+echo "Comparing:\n\t $DIR_A\n\t $DIR_B"
+
 # First, the details
-for i in `ls -1 template/include/`; do
-    diff -u template/include/$i src/include/$i
+for i in `ls -1 $DIR_A/`; do
+    diff -u $DIR_A/$i $DIR_B/$i
 done
 
 # Then, summarize
-for i in `ls -1 template/include/`; do
-    diff -q template/include/$i src/include/$i
+for i in `ls -1 $DIR_A/`; do
+    diff -q $DIR_A/$i $DIR_B/$i
 done
